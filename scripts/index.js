0fd86b128695c32a049b071d736e1d806c6c857b
Nastya Petrova <npetrova.msk@gmail.com>
2022-06-09 01:36:31 +0300

    review fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,31 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
-const saveButton = document.querySelector('.popup__save-button');
 let formElement = document.querySelector('.popup__content');
 let nameInput = document.querySelector('.popup__profile-name');
 let jobInput = document.querySelector('.popup__profile-description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let likeButton = document.querySelector('.element__like-button');
+
+function closePopUp () {
+    popup.classList.remove('popup_opened');
+};
 
 editButton.addEventListener('click', function() {
-    popup.classList.remove('popup_hidden');
+    popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileDescription.textContent;
 });
 
-closeButton.addEventListener('click', function(){
-    popup.classList.add('popup_hidden')
-}
-);
+closeButton.addEventListener('click', closePopUp);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileDescription.textContent = jobInput.value;
-}
+    closePopUp();
+};
 
 formElement.addEventListener('submit', formSubmitHandler); 
 
-saveButton.addEventListener('click', formSubmitHandler);
 
-saveButton.addEventListener('click', function(){
-    popup.classList.add('popup_hidden')
-}
-);
